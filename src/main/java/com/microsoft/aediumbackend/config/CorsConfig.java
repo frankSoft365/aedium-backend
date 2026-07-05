142cb0bcd99d5402a07d12be12a09c28f0f09762
frank <264430662@qq.com>
2026-07-06 01:39:13 +0800

    config cors

--- src/main/java/com/microsoft/aediumbackend/config/CorsConfig.java
+++ src/main/java/com/microsoft/aediumbackend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://192.168.1.13:5173")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600)
